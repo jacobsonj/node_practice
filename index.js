7c7464d1fb34f2c03cf40084fd1de9ba1284cab3
Justin Jacobson <justinjacobson0118@gmail.com>
2019-05-06 20:51:29 -0600

    got delete and post to work

--- index.js
+++ index.js
@@ -1,45 +1,25 @@
 const express = require('express');
-const app = express();
 const PORT = 8080;
 const cors = require('cors')
 const bodyParser = require('body-parser');
-let mountain = [];
+const { Router } = require('express');
+const Mountain = require('./mountain');
+const User = require('./user');
 
+const app = Router();
 
 app.use(cors());
 app.use(bodyParser.json());
 
+app.use('/mountain', Mountain);
+app.use('/user', User);
 
-app.get('/', (req,res) => res.json(mountain));
 
-app.post('/', (req,res) => {
-  mountain.push(req.body);
-  console.log(req.body);
-  res.json(mountain);
-})
 
-// const server = http.createServer((request, response) => { 
-//     // return response.end(dogs);
-    
-//     if (request.url === "/dogs") {
-//       if (request.method === "GET") {
-//         response.end(JSON.stringify(dogs));
-//       } else if (request.method === "POST") {
-//         let body = [];
-//         request.on('data', chunk => {
-//           body.push(chunk);
-//         });
-//         request.on('end', () => {
-//           dogs.push(JSON.parse(body));
-//           response.end(JSON.stringify(dogs));
-//         })
-        
-//       } else {
-//         // others... (PUT, DELETE, etc...)
-//       } 
-//     } else {
-//       // any other route...
-//     }
-//   });
+
+
+
+
+
 
 app.listen(PORT, () => console.log("Server is listening on port %s", PORT));
---
+++ mountain.js
@@ -0,0 +1,23 @@
+const uuid = require('uuid');
+let mountains = [];
+
+app.get('/', (req,res) => res.json(mountains));
+
+app.post('/', (req,res) => {
+  const newMountain = (req.body);
+  const id = uuid();
+  const addId = {...newMountain, id: id}
+  mountains.push(addId);
+  
+  res.json(mountains);
+})
+
+app.delete('/:id', (req,res) => {
+  const id = req.params.id;
+  const idx = mountains.findIndex(mountain => mountain.id === id);
+  const removedMountain = mountains[idx];
+  mountains.splice(idx, 1);
+  
+  console.log(removedMountain);
+  res.json(removedMountain);
+})
